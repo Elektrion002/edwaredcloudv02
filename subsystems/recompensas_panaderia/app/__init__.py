e01906cf4739211791e6f64435f09dcb2decedcf
edwared <elektrion@gmail.com>
2026-03-06 06:27:39 -0600

    Fix Subsistema 01 routing: Add ProxyFix and X-Forwarded-Prefix for subfolder support.

--- subsystems/recompensas_panaderia/app/__init__.py
+++ subsystems/recompensas_panaderia/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -10,6 +11,8 @@ login_manager.login_view = 'auth.login'
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     db.init_app(app)
     login_manager.init_app(app)
